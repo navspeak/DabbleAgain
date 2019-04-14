471c8e51f202d713b0f7dd34c358c96995cea05e
navspeak <navneet.sahay@gmail.com>
2019-04-14 12:50:52 +0530

    Rod Cutting

--- src/main/java/dp/_1RodCutting.java
+++ src/main/java/dp/_1RodCutting.java
@@ -13,6 +13,57 @@ public class _1RodCutting {
         Integer memo[] = new Integer[prices1.length];
         int profit = cut_Recursively(prices1, prices1.length, memo);
         System.out.println(profit == 22 ? "Passed" : "Failed. Expected 22. Got " + profit);
+
+        profit = cut_DP(prices1, prices1.length);
+        System.out.println(profit == 22 ? "Passed" : "Failed. Expected 22. Got " + profit);
+
+
+        /* Variation of above where length isn't same as size of array */
+        /* Meaning we might have rod of length 6 and we can cut it into say
+           1m (2$), 2m(5$), 3m(7$), 4m(3$)
+         */
+        int[] prices2 = new int[]{2, 5, 7, 3};
+        int len = 5;
+
+        profit = cut_DP(prices2, 5);
+        System.out.println(profit == 12 ? "Passed" : "Failed. Expected 12. Got " + profit);
+
+    }
+
+    public static int cut_DP(int[] prices, int length) {
+        int[][]dp = new int[prices.length + 1][length + 1];
+
+        /*
+                0 1 2 3 4 5 6
+            0 0 0 0 0 0 0 0 0
+            1 2 0 2 4 6 8 10 12
+            2 5 0 2 5 7 10 12 15
+            3 7 0 2 5 7 10 12 17
+            4 3 0 2 5 7 10 12
+         */
+        for (int i = 1; i <= prices.length ; i++) {
+            for (int j = 1; j <= length; j++) {
+                int p1 = j>=i? prices[i-1] + dp[i][j-i] : 0;
+                int p2 = dp[i-1][j];
+                dp[i][j] = Math.max(p1, p2);
+            }
+        }
+        printParticipants(prices, length, dp);
+        return dp[prices.length][length];
+    }
+
+    private static void printParticipants(int[] prices, int length, int[][] dp) {
+        int row = prices.length;
+        int col = length;
+        int profit = dp[row][col];
+        while(row>0 && col>0){
+            if (dp[row][col] != dp[row -1][col]){
+                System.out.print("{Length = " + row + " , Price = " + prices[row-1] + "}, ");
+                col = col - row;
+            } else
+                row--;
+        }
+        System.out.print(" Total Profit => " + dp[prices.length][length] + "\n");
     }
 
     // Time complexity = O(n^2) with memo - otherwise O(2^n) - see NOTE
@@ -23,6 +74,7 @@ public class _1RodCutting {
     // because there are n − 1 places where we can choose to make cuts, and at
     // each place, we either make a cut or we do not make a cut.
     // https://web.stanford.edu/class/archive/cs/cs161/cs161.1168/lecture12.pdf
+    //https://www.youtube.com/watch?v=ElFrskby_7M
     private static int cut_Recursively(int[] prices, int length, Integer[] memo) {
         if (length == 0) return 0;
         if (memo[length - 1]!= null) return memo[length - 1];
