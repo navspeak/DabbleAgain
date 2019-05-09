0517943ed7589f0aa039df0f6d8dcce9dde9ffb4
navspeak <navneet.sahay@gmail.com>
2019-05-09 08:44:45 +0530

    interpreter

--- src/main/java/designPattern/behavorial/IteratorPattern.java
+++ src/main/java/designPattern/behavorial/IteratorPattern.java
@@ -1,4 +1,74 @@
 package designPattern.behavorial;
 
+import java.util.Iterator;
+
 public class IteratorPattern {
+    public static void main(String[] args) {
+        BikeRepository repo = new BikeRepository();
+        repo.addBike("Cervelo");
+        repo.addBike("Scott");
+        repo.addBike("Fuji");
+
+        Iterator<String> bikeIterator = repo.iterator();
+
+        //while(bikeIterator.hasNext()) {
+        //	System.out.println(bikeIterator.next());
+        //}
+
+        for (String bike : repo) {
+            System.out.println(bike);
+        }
+    }
+}
+
+
+
+
+class BikeRepository implements Iterable<String> {
+
+    private String [] bikes;
+    private int index;
+
+    public BikeRepository() {
+        bikes = new String[10];
+        index = 0;
+    }
+
+    public void addBike(String bike) {
+        if(index == bikes.length) {
+            String [] largerBikes = new String[bikes.length + 5];
+            System.arraycopy( bikes, 0, largerBikes, 0, bikes.length );
+            bikes = largerBikes;
+            largerBikes = null;
+        }
+
+        bikes[index] = bike;
+        index++;
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        Iterator<String> it = new Iterator<String>() {
+
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < bikes.length && bikes[currentIndex] != null;
+            }
+
+            @Override
+            public String next() {
+                return bikes[currentIndex++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+
+        };
+
+        return it;
+    }
 }
